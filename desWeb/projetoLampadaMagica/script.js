b0ca26c3a530614fa5bdd6d41ad759e05073b55b
Ana Lazdenas <analazdenas@gmail.com>
2023-10-10 21:35:59 -0300

    aula9

--- desWeb/projetoLampadaMagica/script.js
+++ desWeb/projetoLampadaMagica/script.js
@@ -1,4 +1,37 @@
 let lampada=document.getElementById("lampada");
+
+document.getElementById("titulo").style.color="red";
+
 function ligar(){
     lampada.src="luzLigada.gif";
 }
+
+function desligar(){
+    lampada.setAttribute("src", "luzDesligada.gif");
+}
+
+function sumir(){
+    lampada.style.display = "none";
+    //lampada.hidden = true;
+}
+
+function aparecer(){
+    lampada.style.display = "inline";
+    //lampada.hidden = false;
+}
+
+function alternar(){
+    if (lampada.getAttribute("src") == "luzLigada.gif")
+    {
+    desligar();}
+
+    else if(lampada.getAttribute("src") == "luzDesligada.gif")
+    {
+    ligar();}
+}
+
+function piscar(){
+    alternar()
+}
+setInterval(piscar, 500);
+
