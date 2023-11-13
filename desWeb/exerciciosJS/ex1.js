c6ddf1f554bd5f7d7dd17a88793a725a57d7f0a0
Ana Lazdenas <analazdenas@gmail.com>
2023-11-12 21:24:27 -0300

    js

--- desWeb/exerciciosJS/ex1.js
+++ desWeb/exerciciosJS/ex1.js
@@ -1,12 +1,9 @@
 function calcularAumento() {
-    // Obtém os valores informados pelo usuário
     let salario = parseFloat(document.getElementById("salario").value);
     let percentual = parseFloat(document.getElementById("percentual").value);
 
-    // Calcula o novo salário com base no aumento percentual
     let aumento = (salario * percentual) / 100;
     let novoSalario = salario + aumento;
 
-    // Exibe o resultado na página
     document.getElementById("resultado").innerText = (`Seu salário com aumento será R$ ${novoSalario}.`);
 }
